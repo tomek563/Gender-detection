e003a7ddf3a08628cbaede31e2840aae10372e6b
Tomasz <tomek_piatek@wp.pl>
2021-02-26 00:26:42 +0100

    Initial commit

---
+++ src/main/java/piatek/genderdetection/GenderdetectionApplication.java
@@ -0,0 +1,13 @@
+package piatek.genderdetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenderdetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenderdetectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/piatek/genderdetection/GenderdetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package piatek.genderdetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenderdetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
